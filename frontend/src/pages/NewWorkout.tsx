071841a3a863c2e3be5554dda581111a370bc8e0
scothook <scothook@gmail.com>
2025-03-24 14:52:19 -0700

    fix error

--- frontend/src/pages/NewWorkout.tsx
+++ frontend/src/pages/NewWorkout.tsx
@@ -3,14 +3,13 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../components/button/Button";
 import Layout from "../components/layout/Layout";
-import styles from "./Home.module.css";
 
 const NewWorkout: React.FC = () => {
   const navigate = useNavigate();
 
   return (
     <Layout>
-      <h1 className={styles.header}>Start a New Workout</h1>
+      <h1>Start a New Workout</h1>
       <Button label="Back" onClick={() => navigate("/")}/>
     </Layout>
   );
