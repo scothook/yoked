02f44fccc5abbd8b3fef1e13eae630994ff01268
scothook <scot.hook@gmail.com>
2025-04-26 12:58:20 -0700

    0.3.4: makes progress tracker page match others

--- frontend/src/pages/ProgressTracker.tsx
+++ frontend/src/pages/ProgressTracker.tsx
@@ -1,15 +1,45 @@
 // src/pages/Progress.tsx
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import Button from "../components/button/Button";
+import React, { useEffect } from "react";
+//import { useNavigate } from "react-router-dom";
 import Layout from "../components/layout/Layout";
+import PageHeader from "../components/pageHeader/PageHeader";
+import Drawer from '@mui/material/Drawer';
 
 const ProgressTracker: React.FC = () => {
-    const navigate = useNavigate();
+    //const navigate = useNavigate();
+
+    const [loading, setLoading] = React.useState(true);
+    const [error, setError] = React.useState<string | null>(null);
+
+    const [drawerOpen, setDrawerOpen] = React.useState(false);
+    
+      const toggleDrawer = (newDrawerOpen: boolean) => () => {
+        console.log('inside toggleDrawer');
+        setDrawerOpen(newDrawerOpen);
+      };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+              console.log('inside fetchData');
+            } catch (err) {
+                setError(err as string);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+      fetchData();
+    }, []);
+
   return (
     <Layout>
-      <h1>progress tracker</h1>
-      <Button label="← Back" onClick={() => navigate(-1)}/>
+      <PageHeader title="past workouts" cornerTitle="" variant="hamburger" cornerTitleOnClick={toggleDrawer(true)}/>
+            {loading && <p>Loading...</p>}
+            {error && <p>Error: {error}</p>}
+      <Drawer anchor={'right'} open={drawerOpen} onClose={toggleDrawer(false)}>
+      hey
+    </Drawer>
   </Layout>
   )
 };
